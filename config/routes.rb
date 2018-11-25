e51ecb966bbf06aba5d23fc130b043c0c8323520
wanabe <s.wanabe@gmail.com>
2018-11-25 21:27:12 +0900

    chore: run `bundle exec rails generate graphql:install`

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 ActiveRecordVisualizer::Engine.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   root to: "front_end#index"
 end

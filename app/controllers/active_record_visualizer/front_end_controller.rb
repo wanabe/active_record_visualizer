875aafb3b4987271892754b4ee2d4a62139f20f0
wanabe <s.wanabe@gmail.com>
2018-11-25 18:40:29 +0900

    feat(FrontEndController#index): render with Vue.js

--- app/controllers/active_record_visualizer/front_end_controller.rb
+++ app/controllers/active_record_visualizer/front_end_controller.rb
@@ -3,7 +3,6 @@ require_dependency "active_record_visualizer/application_controller"
 module ActiveRecordVisualizer
   class FrontEndController < ApplicationController
     def index
-      render body: nil
     end
   end
 end

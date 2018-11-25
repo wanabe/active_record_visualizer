dd6b85487fbc07ea34326a755a74033ece1c2964
wanabe <s.wanabe@gmail.com>
2018-11-26 08:11:33 +0900

    feat: call GraphQL API from vue

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,61 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <button @click="load()">Load</button>
+    <ul>
+      <li v-for="modelClass in modelClasses.nodes">
+        <input type="checkbox" name="model_class" v-bind:value="modelClass.name" checked="checked"></input>
+        {{ modelClass.name }}
+        <ul>
+          <li v-for="association in modelClass.associations.nodes">
+            <input type="checkbox" name="model_association" v-bind:value="modelClass.name + '-' + association.name" checked="checked"></input>
+            {{ association.name }} -> {{ association.klass }}
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <pre>{{ raw }}</pre>
   </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
+  import gql from 'graphql-tag'
+
+  export default {
+    data: function () {
+      return {
+        raw: '',
+        modelClasses: {}
+      }
+    },
+    methods: {
+      load () {
+        const QUERY = gql`
+          query($exceptClasses: [String!], $exceptAssociations: [String!]) {
+            modelClasses(except: $exceptClasses) {
+              nodes {
+                name
+                associations(exceptClasses: $exceptClasses, except: $exceptAssociations) {
+                  nodes {
+                    name
+                    klass
+                  }
+                }
+              }
+            }
+          }
+        `
+        this.$apollo.query({
+          query: QUERY
+        }).then((result) => {
+          this.$data.raw = result.data
+          this.$data.modelClasses = result.data.modelClasses
+        }).catch((error) => {
+          alert(error)
+        })
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
 </style>
--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from '../app.vue'
+import ApolloClient from "apollo-boost"
+import VueApollo from "vue-apollo"
+
+Vue.use(VueApollo)
+
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:3001/active_record_visualizer/graphql"
+})
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.body.appendChild(document.createElement('hello'))
   const app = new Vue({
     el,
+    apolloProvider,
     render: h => h(App)
   })
 

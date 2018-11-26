167c7226414c94da1b78056f845dfcc27bfcae30
wanabe <s.wanabe@gmail.com>
2018-11-27 08:49:00 +0900

    feat(config/routes.rb): drop to mount GraphiQL::Rails::Engine because application should do

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 ActiveRecordVisualizer::Engine.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/active_record_visualizer/graphql"
-  end
   post "/graphql", to: "graphql#execute"
   root to: "front_end#index"
 end
